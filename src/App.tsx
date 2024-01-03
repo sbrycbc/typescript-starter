2f46e8762052e847fedfae91c9634ce8eda82385
sbrycbc <sabriyecbc@gmail.com>
2024-01-03 11:52:37 +0100

    learning basic first...

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,60 @@
-import React from 'react';
 import './App.css';
 
-function App() {
+function App(){
   return (
-    <div className="App">
-     Typescript Notes
-    </div>
-  );
+  <div className='App'>
+    Typescript Notes
+  </div>
+  )
 }
 
 export default App;
+
+
+
+
+
+
+// function App() {
+//   let name: string = "Typescript Notes";
+//   let age: number= 37;
+//   //let done: boolean = true;
+//   let trial: any = "test testt";
+//   let beide: number | string = 5
+
+//   /* type Obj = {
+//     name: string,
+//     age: number,
+//     done?:boolean
+//   }
+
+//   let obj: Obj = {
+//     name: "Typescript Notes",
+//     age: 26,
+//     done:true
+//   } */
+
+//   interface Obj {
+//     name: string,
+//     age: number,
+//     done?:boolean
+//   }
+
+//   interface ObjTwo extends Obj {
+//     accept?: boolean
+//   }
+
+//   let obj: ObjTwo = {
+//     name: "Typescript Notes",
+//     age: 26,
+//     done:true
+//   }
+
+//   return (
+//     <div className="App">
+//      {name + " - " + age + " mit " + trial + " oder " + beide }
+//     </div>
+//   );
+// }
+
+
