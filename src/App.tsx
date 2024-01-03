9e19fc141f9f9a200c2f666bd38acb3666367e45
sbrycbc <sabriyecbc@gmail.com>
2024-01-03 13:52:44 +0100

    added message...

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { useState } from 'react';
 import './App.css';
+import Input from './components/Input';
+import { Todos } from './types/Type';
+import Message from './components/Message';
 
-function App(){
+const App = () => {
+  const [todo, setTodo] = useState<string>('')
+  const [todos, setTodos] = useState<Todos[]>([])
+
+
+  const addMessage = ()=> {
+    if(todo) setTodos([...todos,{message:todo, id: todos.length + 1}])
+    setTodo('')
+  }
+
+  const deleteMessage = (id:number) => {
+    setTodos(todos.filter(todo => todo.id != id))
+  }
+
   return (
   <div className='App'>
-    Typescript Notes
+   <Input addMessage = {addMessage}todo={todo} setTodo = {setTodo}/>
+   <Message deleteMessage={deleteMessage} todos={todos}/>
   </div>
   )
 }
